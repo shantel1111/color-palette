5c8fe28de9935f1b3062745f282763b1cbd124b8
shantel1111 <148761452+shantel1111@users.noreply.github.com>
2025-08-28 12:44:23 -0400

    Organised JS file

--- index.js
+++ index.js
@@ -2,25 +2,32 @@ let colorPicker = document.getElementById("color-picker") // input
 let colorChoices = document.getElementById("color-choices") // select
 const colorPalette = document.getElementById("color-palette")
 const btnEl = document.getElementById("btn-el") // submit
+let html = ''
 
 btnEl.addEventListener("click", function(){
-    let html = ''
+    html = ''
     let chosenColor = colorPicker.value
-    let noHex = chosenColor.slice(1,chosenColor.length)
+    let noHex = chosenColor.slice(1)
 
     fetch(`https://www.thecolorapi.com/scheme?hex=${noHex}&mode=${colorChoices.value}&count=5`)
     .then(response => response.json())
     .then(data => {
-        for (let color of data.colors){
-            console.log(color.hex.value)
-        html += `
-        <div class="color-section">
-        <div class="color-column" style="background-color: ${color.hex.value}"></div>
-        <p class="hex-box">${color.hex.value}</p>
-        </div>`
-    }
-    colorPalette.innerHTML = html
-
+        htmlString(data)
+        render()
     })
 })
+
+function htmlString(data){
+    data.colors.forEach(function(color){
+          html += `
+            <div class="color-section">
+              <div class="color-column" style="background-color: ${color.hex.value}"></div>
+              <p class="hex-box">${color.hex.value}</p>
+            </div>`
+    })
+}
+
+function render(){
+  colorPalette.innerHTML = html
+}
 
