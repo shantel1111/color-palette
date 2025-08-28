f63220926aaa2d9d8e8289bfb983ea021b01904e
shantel1111 <148761452+shantel1111@users.noreply.github.com>
2025-08-28 13:10:10 -0400

    Improved formatting

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-let colorPicker = document.getElementById("color-picker") // input
-let colorChoices = document.getElementById("color-choices") // select
-const colorPalette = document.getElementById("color-palette")
-const btnEl = document.getElementById("btn-el") // submit
+let colorPicker = document.getElementById("color-picker"); // input
+let colorChoices = document.getElementById("color-choices"); // select
+const colorPalette = document.getElementById("color-palette");
+const btnEl = document.getElementById("btn-el"); // submit
 
 btnEl.addEventListener("click", () => {
-    let chosenColor = colorPicker.value
-    let noHex = chosenColor.slice(1)
+  let chosenColor = colorPicker.value;
+  let noHex = chosenColor.slice(1);
 
-    fetch(`https://www.thecolorapi.com/scheme?hex=${noHex}&mode=${colorChoices.value}&count=5`)
-    .then(response => response.json())
-    .then(data => displayColors(data))
-})
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${noHex}&mode=${colorChoices.value}&count=5`
+  )
+    .then((response) => response.json())
+    .then((data) => displayColors(data));
+});
 
-function displayColors(data){
-    let html = ""
-    data.colors.forEach (color => {
-          html += `
+function displayColors(data) {
+  let html = "";
+  data.colors.forEach((color) => {
+    html += `
             <div class="color-section">
               <div class="color-column" style="background-color: ${color.hex.value}"></div>
               <p class="hex-box">${color.hex.value}</p>
-            </div>`
-    })
-    colorPalette.innerHTML = html
+            </div>`;
+  });
+  colorPalette.innerHTML = html;
 }
