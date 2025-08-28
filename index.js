8b639253ce0eb7d2ed9b65030079bf3ddbc53225
shantel1111 <148761452+shantel1111@users.noreply.github.com>
2025-08-28 12:59:06 -0400

    Simplified JS

--- index.js
+++ index.js
@@ -2,32 +2,24 @@ let colorPicker = document.getElementById("color-picker") // input
 let colorChoices = document.getElementById("color-choices") // select
 const colorPalette = document.getElementById("color-palette")
 const btnEl = document.getElementById("btn-el") // submit
-let html = ''
 
-btnEl.addEventListener("click", function(){
-    html = ''
+btnEl.addEventListener("click", () => {
     let chosenColor = colorPicker.value
     let noHex = chosenColor.slice(1)
 
     fetch(`https://www.thecolorapi.com/scheme?hex=${noHex}&mode=${colorChoices.value}&count=5`)
     .then(response => response.json())
-    .then(data => {
-        htmlString(data)
-        render()
-    })
+    .then(data => displayColors(data))
 })
 
-function htmlString(data){
-    data.colors.forEach(function(color){
+function displayColors(data){
+    let html = ""
+    data.colors.forEach (color => {
           html += `
             <div class="color-section">
               <div class="color-column" style="background-color: ${color.hex.value}"></div>
               <p class="hex-box">${color.hex.value}</p>
             </div>`
     })
+    colorPalette.innerHTML = html
 }
-
-function render(){
-  colorPalette.innerHTML = html
-}
-
